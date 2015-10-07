ff157c5ccf61907e1b042aba60b73c2860c7f084
dfm555 <dmontoya@xumak.com>
2015-10-07 14:29:38 -0500

    add grid to panel table

--- src/views/Window.java
+++ src/views/Window.java
@@ -8,19 +8,23 @@ import java.awt.*;
  */
 public class Window extends JFrame {
 
+    private ElementsPanel elementsPanel = new ElementsPanel();
     private TablePanel tablePanel = new TablePanel();
+    private ButtonsPanel buttonsPanel = new ButtonsPanel();
 
     public Window(){
         initComponents();
     }
 
     public void initComponents() {
-
-        setTitle(" Students Information ");
+        setTitle("Students Information");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
         setSize(500, 500);
-        setVisible(true);
-        setLayout(new GridLayout(3, 1));
+        setLocationRelativeTo(null);
+        //add(tablePanel, BorderLayout.NORTH);
         add(tablePanel, BorderLayout.CENTER);
+        //add(tablePanel, BorderLayout.SOUTH);
     }
 
 }
