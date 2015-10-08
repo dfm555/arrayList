450ef05e7bcba71f868222a310718007d4a50917
dfm555 <dmontoya@xumak.com>
2015-10-08 11:27:56 -0500

    create list

--- src/views/TablePanel.java
+++ src/views/TablePanel.java
@@ -1,8 +1,11 @@
 package views;
 
-import javax.swing.*;
-import java.awt.*;
+import controllers.Students;
 
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.util.List;
 /**
  * Created by monicaramirez on 5/10/15.
  */
@@ -10,59 +13,41 @@ public class TablePanel extends JPanel {
 
     //private DefaultTableModel tableModel;
     private JTable dataGrid;
+    private Students students;
 
-    public TablePanel() {
+    public TablePanel( Students students) {
         initComponents();
+        this.students = students;
+        System.out.print( this.students.showStudents() );
     }
 
     public void initComponents() {
         setBorder(BorderFactory.createTitledBorder("Data grid"));
         setLayout(new BorderLayout() );
-        String[] col = {
-                "ID Student",
-                "Name",
-                "Career",
-                "Approve",
-                "Average"
-        };
-        Object object[][] = {
-                { 111, "Duber", "Sistemas", 6, 4.5 },
-                { 111, "Duber", "Sistemas", 6, 4.5 },
-                { 111, "Duber", "Sistemas", 6, 4.5 },
-                { 111, "Duber", "Sistemas", 6, 4.5 },
-                { 111, "Duber", "Sistemas", 6, 4.5 },
-                { 111, "Duber", "Sistemas", 6, 4.5 },
-                { 111, "Duber", "Sistemas", 6, 4.5 },
-                { 111, "Duber", "Sistemas", 6, 4.5 },
-                { 111, "Duber", "Sistemas", 6, 4.5 },
-                { 111, "Duber", "Sistemas", 6, 4.5 },
-                { 111, "Duber", "Sistemas", 6, 4.5 },
-                { 111, "Duber", "Sistemas", 6, 4.5 },
-                { 111, "Duber", "Sistemas", 6, 4.5 },
-                { 111, "Duber", "Sistemas", 6, 4.5 },
-                { 111, "Duber", "Sistemas", 6, 4.5 },
-                { 111, "Duber", "Sistemas", 6, 4.5 },
-                { 111, "Duber", "Sistemas", 6, 4.5 },
-                { 111, "Duber", "Sistemas", 6, 4.5 },
-                { 111, "Duber", "Sistemas", 6, 4.5 },
-                { 111, "Duber", "Sistemas", 6, 4.5 },
-                { 111, "Duber", "Sistemas", 6, 4.5 },
-                { 111, "Duber", "Sistemas", 6, 4.5 },
-                { 111, "Duber", "Sistemas", 6, 4.5 },
-                { 111, "Duber", "Sistemas", 6, 4.5 },
-                { 111, "Duber", "Sistemas", 6, 4.5 },
-                { 111, "Duber", "Sistemas", 6, 4.5 },
-                { 111, "Duber", "Sistemas", 6, 4.5 },
-                { 111, "Duber", "Sistemas", 6, 4.5 },
-
-
-
-        };
-
-        dataGrid = new JTable( object, col );
-
-        JScrollPane tableContainer = new JScrollPane(dataGrid);
-        tableContainer.setPreferredSize(new Dimension(500, 200));
-        add( tableContainer, BorderLayout.CENTER );
+//        String[] col = {
+//                "ID Student",
+//                "Name",
+//                "Career",
+//                "Approved",
+//                "Average"
+//        };
+//
+//        DefaultTableModel tableModel = new DefaultTableModel(col, 0);
+//
+//        for (int i = 0; i < students.showStudents().size(); i++){
+//            int id = students.showStudents().get(i).getId();
+//            String name = students.showStudents().get(i).getName();
+//            String career = students.showStudents().get(i).getCareer();
+//            int creditsApproved = students.showStudents().get(i).getNumberCreditsApprove();
+//            double average = students.showStudents().get(i).getAverage();
+//            Object[] data = { id, name, career, creditsApproved, average };
+//            tableModel.addRow(data);
+//        }
+//
+//        dataGrid = new JTable( tableModel );
+//
+//        JScrollPane tableContainer = new JScrollPane(dataGrid);
+//        tableContainer.setPreferredSize(new Dimension(500, 200));
+//        add( tableContainer, BorderLayout.CENTER );
     }
 }
--- src/views/Window.java
+++ src/views/Window.java
@@ -10,12 +10,19 @@ import java.awt.*;
  */
 public class Window extends JFrame {
 
+    private Students students = new Students();
     private ElementsPanel elementsPanel = new ElementsPanel();
-    private TablePanel tablePanel = new TablePanel();
+    private TablePanel tablePanel = new TablePanel( students );
     private ButtonsPanel buttonsPanel = new ButtonsPanel( this );
 
     public Window(){
         initComponents();
+        students.setId(1);
+        students.setName("Name test");
+        students.setCareer("Career test");
+        students.setNumberCreditsApprove(40);
+        students.setAverage(4.5);
+        students.saveStudent(students);
     }
 
     public void initComponents() {
@@ -34,10 +41,26 @@ public class Window extends JFrame {
     public void executeAction( String action ){
         switch (action){
             case "save":
-                Students students = new Students();
+                String name = elementsPanel.getTxtName().getText();
+                String career = elementsPanel.getTxtCareer().getText();
+                int creditsApproved = Integer.parseInt( elementsPanel.getTxtNumberCreditApprove().getText() );
+                double average = Double.parseDouble( elementsPanel.getTxtAverage().getText() );
+                int id = students.showStudents().size() + 1;
+                students.setId(id);
+                students.setName(name);
+                students.setCareer(career);
+                students.setNumberCreditsApprove(creditsApproved);
+                students.setAverage(average);
+                boolean result = students.saveStudent(students);
+                if(result){
+                    JOptionPane.showMessageDialog(null, "Success");
+                }else {
+                    JOptionPane.showMessageDialog(null, "Error");
+                }
                 break;
         }
 
     }
 
 }
+
