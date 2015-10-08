0a6d53308c24b0d43ab78c66bc1463b39c0f1a19
dfm555 <dmontoya@xumak.com>
2015-10-08 13:16:46 -0500

    add construct

--- src/views/TablePanel.java
+++ src/views/TablePanel.java
@@ -15,39 +15,52 @@ public class TablePanel extends JPanel {
     private JTable dataGrid;
     private Students students;
 
+    public TablePanel(){
+
+    }
+
     public TablePanel( Students students) {
-        initComponents();
         this.students = students;
-        System.out.print( this.students.showStudents() );
+        initComponents();
+    }
+
+    public JTable getDataGrid() {
+        return dataGrid;
+    }
+
+    public TablePanel setDataGrid(JTable dataGrid) {
+        this.dataGrid = dataGrid;
+        return this;
     }
 
     public void initComponents() {
         setBorder(BorderFactory.createTitledBorder("Data grid"));
         setLayout(new BorderLayout() );
-//        String[] col = {
-//                "ID Student",
-//                "Name",
-//                "Career",
-//                "Approved",
-//                "Average"
-//        };
-//
-//        DefaultTableModel tableModel = new DefaultTableModel(col, 0);
-//
-//        for (int i = 0; i < students.showStudents().size(); i++){
-//            int id = students.showStudents().get(i).getId();
-//            String name = students.showStudents().get(i).getName();
-//            String career = students.showStudents().get(i).getCareer();
-//            int creditsApproved = students.showStudents().get(i).getNumberCreditsApprove();
-//            double average = students.showStudents().get(i).getAverage();
-//            Object[] data = { id, name, career, creditsApproved, average };
-//            tableModel.addRow(data);
-//        }
-//
-//        dataGrid = new JTable( tableModel );
-//
-//        JScrollPane tableContainer = new JScrollPane(dataGrid);
-//        tableContainer.setPreferredSize(new Dimension(500, 200));
-//        add( tableContainer, BorderLayout.CENTER );
+        setDataGrid(new JTable());
+        String[] col = {
+                "ID Student",
+                "Name",
+                "Career",
+                "Approved",
+                "Average"
+        };
+
+        DefaultTableModel tableModel = new DefaultTableModel(col, 0);
+
+        for (int i = 0; i < this.students.showStudents().size(); i++){
+            int id = students.showStudents().get(i).getId();
+            String name = students.showStudents().get(i).getName();
+            String career = students.showStudents().get(i).getCareer();
+            int creditsApproved = students.showStudents().get(i).getNumberCreditsApprove();
+            double average = students.showStudents().get(i).getAverage();
+            Object[] data = { id, name, career, creditsApproved, average };
+            tableModel.addRow(data);
+        }
+
+        getDataGrid().setModel(tableModel);
+
+        JScrollPane tableContainer = new JScrollPane(getDataGrid());
+        tableContainer.setPreferredSize(new Dimension(500, 200));
+        add( tableContainer, BorderLayout.CENTER );
     }
 }
--- src/views/Window.java
+++ src/views/Window.java
@@ -3,6 +3,7 @@ package views;
 import controllers.Students;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 
 /**
@@ -12,20 +13,20 @@ public class Window extends JFrame {
 
     private Students students = new Students();
     private ElementsPanel elementsPanel = new ElementsPanel();
-    private TablePanel tablePanel = new TablePanel( students );
+    private TablePanel tablePanel = new TablePanel();
     private ButtonsPanel buttonsPanel = new ButtonsPanel( this );
 
     public Window(){
         initComponents();
+    }
+
+    public void initComponents() {
         students.setId(1);
         students.setName("Name test");
         students.setCareer("Career test");
         students.setNumberCreditsApprove(40);
         students.setAverage(4.5);
-        students.saveStudent(students);
-    }
-
-    public void initComponents() {
+        tablePanel = new TablePanel(students);
         setTitle("Students Information");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
@@ -53,13 +54,46 @@ public class Window extends JFrame {
                 students.setAverage(average);
                 boolean result = students.saveStudent(students);
                 if(result){
-                    JOptionPane.showMessageDialog(null, "Success");
+                    refreshGrid();
                 }else {
                     JOptionPane.showMessageDialog(null, "Error");
                 }
                 break;
         }
 
+    }
+
+    public void refreshGrid (){
+        //System.out.print( students.showStudents().size() );
+        JOptionPane.showMessageDialog(null, "Success");
+        String[] col = {
+                "ID Student",
+                "Name",
+                "Career",
+                "Approved",
+                "Average"
+        };
+
+        DefaultTableModel tableModel = new DefaultTableModel(col, 0);
+
+        for (int i = 0; i < students.showStudents().size(); i++){
+            int id = students.showStudents().get(i).getId();
+            String name = students.showStudents().get(i).getName();
+            String career = students.showStudents().get(i).getCareer();
+            int creditsApproved = students.showStudents().get(i).getNumberCreditsApprove();
+            double average = students.showStudents().get(i).getAverage();
+            Object[] data = { id, name, career, creditsApproved, average };
+            tableModel.addRow(data);
+        }
+
+        for (int i = 0; i < students.showStudents().size(); i++){
+            System.out.print(students.showStudents().get(i).getId());
+            System.out.print(students.showStudents().get(i).getName());
+            System.out.print(students.showStudents().get(i).getCareer());
+            System.out.print("\n");
+        }
+
+        //tablePanel.getDataGrid().setModel(tableModel);
     }
 
 }
